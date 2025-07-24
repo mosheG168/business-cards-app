ef00be0c2ca8a7e085480736a638af29764ad889
mosheG168 <m.green4903@gmail.com>
2025-07-24 23:20:48 +0300

    added

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <>
       <Header />
-
+      
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<RegisterPage />} />
