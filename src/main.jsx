9a311fe9534cb6bc754f7b68467d2172f7664a2c
mosheG168 <m.green4903@gmail.com>
2025-07-24 21:00:13 +0300

    added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
@@ -17,13 +18,13 @@ const ThemedApp = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
+      <HashRouter>
         <SnackbarProvider>
           <UserProvider>
             <App />
           </UserProvider>
         </SnackbarProvider>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   );
 };
